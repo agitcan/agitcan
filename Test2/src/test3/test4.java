e89b56c7aa3ecb1088e3ed96df64a51c77c489c8
agitcan <agitcan13@gmail.com>
2022-04-12 01:01:53 -0400

    firstcommit

---
+++ Test2/src/test3/test4.java
@@ -0,0 +1,12 @@
+package test3;
+
+public class test4 {
+    public static void main(String[] args) {
+        System.out.println("hello gardas");
+        System.out.println("hello gardas");
+        System.out.println("hello gardas");
+        System.out.println("hello gardas");
+        System.out.println("hello gardas");
+
+    }
+}
